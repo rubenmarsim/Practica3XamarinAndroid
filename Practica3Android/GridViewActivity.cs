46ca28eb578f1a04c32f0467111b306e97bbce7d
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2018-11-09 16:44:51 +0100

    add gridview abecedario

--- Practica3Android/GridViewActivity.cs
+++ Practica3Android/GridViewActivity.cs
@@ -9,6 +9,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Practica3Android.Adapter;
 
 namespace Practica3Android
 {
@@ -17,7 +18,7 @@ namespace Practica3Android
     {
         string[] array_character =
         {
-            "a","b","c"
+            "a","b","c","d","e","f","g"
         };
         List<string> lstResult = new List<string>();
         protected override void OnCreate(Bundle savedInstanceState)
@@ -28,6 +29,8 @@ namespace Practica3Android
             SetContentView(Resource.Layout.GridView);
             SetupList();
             GridView gridView = FindViewById<GridView>(Resource.Id.gridView1);
+            var gridViewAdapter = new GridViewAdapter(lstResult, this);
+            gridView.Adapter = gridViewAdapter;
         }
         private void SetupList()
         {
