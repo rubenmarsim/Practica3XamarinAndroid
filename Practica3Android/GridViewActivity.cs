5d938f65f5aea5b7e35552619ed486d9cb4da4f4
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2018-11-09 16:03:41 +0100

    Update Practica3Android/GridViewActivity.cs

--- Practica3Android/GridViewActivity.cs
+++ Practica3Android/GridViewActivity.cs
@@ -15,12 +15,24 @@ namespace Practica3Android
     [Activity(Label = "GridViewActivity")]
     public class GridViewActivity : Activity
     {
+        string[] array_character =
+        {
+            "a","b","c"
+        };
+        List<string> lstResult = new List<string>();
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
             SetContentView(Resource.Layout.GridView);
+            SetupList();
+            GridView gridView = FindViewById<GridView>(Resource.Id.gridView1);
+        }
+        private void SetupList()
+        {
+            foreach (var item in array_character)
+                lstResult.Add(item);
         }
     }
 }
